1eaa4f43b21e71d849d3e19a259a83386f44c9a0
Schroedingberg <aaronrebmann@gmail.com>
2025-12-13 21:43:43 +0100

    feat: state reconstruction from event log and display in ui

--- src/rp/app.clj
+++ src/rp/app.clj
@@ -8,10 +8,12 @@
             [rp.domain.state :as st]
             [xtdb.api :as xt]
             [cheshire.core :as cheshire]
-            [clojure.core :as c]))
+            [clojure.core :as c]
+            [clojure.data :as data]))
 
 
-(def example-events [(st/completed-set "Twice a week upper body focus" 0 :monday "Squat" 100 10 nil nil)
+(def example-events [(st/completed-set "Twice a week upper body focus" 0 :monday "Barbell Upright Row" 30 10 nil nil)
+                     (st/completed-set "Twice a week upper body focus" 0 :monday "Squat" 100 10 nil nil)
                      (st/completed-set "Twice a week upper body focus" 0 :monday "Squat" 100 9  nil nil)
                      (st/completed-set "Twice a week upper body focus" 0 :monday "Pullup" 80 10  nil nil)
                      (st/completed-set "Twice a week upper body focus" 0 :thursday "Bench" 100 8 nil nil)
@@ -19,17 +21,11 @@
                      (st/completed-set "Twice a week upper body focus" 0 :thursday "Deadlift" 300 5 nil nil)
                      (st/completed-set "Twice a week upper body focus" 1 :monday "Squat" nil nil 102.5 10)])
 
-(def merged-plan-struct
-  (let [from-events (->> example-events
-                       (st/get-microcycle 0)
-                       st/microcycle->plan-structure)
-      from-template (-> plan/template plan/->plan)]
-  (merge from-template from-events)))
-
-
-
-
-
+;; TODO: Dont define global vars in app.clj
+(def progress-map
+  (-> example-events
+    (st/view-progress-in-plan (plan/->plan plan/template))
+    (ui/render-plan)))
 
 (defn app [{:keys [session biff/db] :as ctx}]
   (let [{:user/keys [email]} (xt/entity db (:uid session))]
@@ -42,9 +38,7 @@
        [:button.text-blue-500.hover:text-blue-800 {:type "submit"}
         "Sign out"])
       "."]
-     [:div "Word"]
-     [:.h-1 "Plan reconstructed from event log"]
-     (ui/render-plan merged-plan-struct)
+     progress-map
      [:.flex-grow
       ]
 
@@ -72,15 +66,5 @@
 
 (comment
 
-  (let [tmplt plan/template
-        plan-from-template (plan/->plan tmplt)]
-    plan-from-template)
-
-  (let [plan-from-events (->> example-events
-       (st/get-microcycle 0)
-       st/microcycle->plan-structure)]
-    plan-from-events)
-  merged-plan-struct
-
-  ();;end of rich comment block
+      ();;end of rich comment block
   )
--- src/rp/domain/state.clj
+++ src/rp/domain/state.clj
@@ -1,9 +1,16 @@
 (ns rp.domain.state
-)
+  "State reconstruction from events.
 
+  This provides functions to create set-completed events
+  and functions to rebuild state from a flat event log.
 
+  The most important is view-progress-in-plan,
+  which you can supply an event-log and a plan and
+  get the progress in the context of the plan.
+  "
+  (:require [rp.util.core :as utils]))
 
-(defn completed-set "Log a completed set with full context."
+(defn completed-set "Create a completed set with full context."
   [mesocycle microcycle workout exercise performed-weight performed-reps prescribed-weight prescribed-reps]
   {:type :set-completed
    :mesocycle mesocycle
@@ -17,52 +24,49 @@
    :timestamp (java.time.Instant/now)})
 
 
+(defn events->plan-map
+  "Transforms a flat event log into a nested map that is congruent with the structure of a plan.
 
+  This can then be used to visualize the progress in the context of a plan with
+  view-progress-in-plan
+  "
+  [events]
+  (as-> events e
+    (group-by (juxt :mesocycle :microcycle :workout) e)
+    (update-vals  e (partial group-by :exercise))
+    (reduce-kv (fn [m k v]
+                 (assoc-in m k v)) {} e)))
 
+(defn merge-sets
+  "Takes two vectors with exercise maps and merges each one of them, by position.
 
-(defn ->set [event]
-  (select-keys event [:weight :reps]))
+  Pads the shorter vector with empty maps.
+  Caution: This relies on the sets being in vectors, because ordering is kind of important here."
+  [from-events from-plan]
+  (let [max-count (max (count from-events) (count from-plan))
+        pad-planned (concat from-plan (repeat {}))
+        pad-performed (concat from-events (repeat {}))]
+    (vec (take max-count (map merge pad-planned pad-performed)))))
 
-(defn ->sets [events]
-  (mapv ->set events))
+;; Public API
+;; TODO: Move internal functions of state reconstruction to private namespace and expose public api via public namespace
 
-(defn get-all-workouts-for-day
-  "Get all instances of a certain workout.
-   That would be e.g. the monday workout,
-   over the whole history.
-  Not sure how useful this is, more of an exercise.
-   "
-  [day events]
-  (->> events
-       (filter #(= (:workout %) day))
-       (group-by (juxt :mesocycle :microcycle :workout :exercise))))
+(defn view-progress-in-plan
+  "Creates a progress view (a map) from a given event log and a plan.
 
-
-
-
-;; Probably have to ditch the :exercise to make this usable for workouts that consist of several exercises
-;; AAAAnd thats what i did. To actually render the "day" for ui i might have to put the vals through another group-by :exercise then
-;; Actually I am doing that directly in this function. This gives us structurally almost what we need to render this in the UI, only need to transform this somehow to make rendering more convenient. Maybe its already possible like this, lets
-;; explore further
-;; Maybe we just need to come up with a better name for this function
-(defn get-microcycle [n events]
-  (let [by-microcycle-and-workout (->> events
-                                       (filter #(= (:microcycle %) n))
-                                       (group-by (juxt :mesocycle :microcycle :workout)))]
-    (update-vals by-microcycle-and-workout (partial group-by :exercise))))
-
-
-(defn microcycle->plan-structure
-  "Transform a microcycle view on the events into a nested map that is congruent
-  with the structure of a plan."
-  [microcycle]
-
-  (reduce-kv (fn [m k v]
-               (assoc-in m k v)) {} microcycle))
+  This just deep-merges plan and event log.
+  From that you can see what was planned, what of it has been done and what
+  exactly has been performed."
+  [event-log plan]
+  (let [from-events (->> event-log
+                         events->plan-map)]
+    (utils/deep-merge-with merge-sets from-events plan)))
 
 
 
 
+
+;; Fiddling comment section
 (comment
 
   (def events
@@ -74,18 +78,55 @@
      (completed-set "full body" 0 :thursday "Deadlift" 300 5 nil nil)
      (completed-set "full body" 1 :monday "Squat" nil nil 102.5 10)])
 
-  (def ^:private new-value
+  (def ^:private from-events
     (->> events
          (get-microcycle 0)
-         microcycle->plan-structure))
+         microcycle->plan-structure
+         vals
+         first))
 
-  new-value
+  from-events
 
+  (def plan-excerpt
+    {0
+     {:monday
+      {"Squat"
+       [{:muscle-groups [:quads], :equipment [:barbell :rack]}
+        {:muscle-groups [:quads], :equipment [:barbell :rack]}],
+       "Pullup"
+       [{:muscle-groups [:back], :equipment [:parallel-bar]}]},
+      :thursday
+      {"Bench"
+       [{:muscle-groups [:chest], :equipment [:bench :barbell]}
+        {:muscle-groups [:chest], :equipment [:bench :barbell]}],
+       "Deadlift"
+       [{:muscle-groups [:hamstrings], :equipment [:barbell]}
+        {:muscle-groups [:hamstrings], :equipment [:barbell]}]}}})
 
+  ;; What we want to do is merge the map that we built from the event log with the plan-map.
+  ;; We've constructed (hopefully) all levels of those maps such that every value is in turn a map,
+  ;; except for the sets of an exercise - which is basically the leaf level of our tree.
+  ;; Those we store as vectors. This is primariy due to the fact that a set cant be really assigned an identity in a meaningful way.
+  ;; Now, if these assumptions hold true, we can do the following on the workout level:
+  (let [wo-from-events (get-in from-events [0 :monday])
+        wo-from-plan   (get-in plan-excerpt [0 :monday])]
+    (merge-with merge-sets wo-from-events wo-from-plan)) ;; Here we just merge monday's workouts with our merge sets function.
 
+;; To do that on the next higher level, so for a full microcycle, we could do this:
+  (let [wo-from-events (get-in from-events [0])
+        wo-from-plan   (get-in plan-excerpt [0])]
+    ;; We're basically taking the previous block and feed it as the function argument to another merge-with
+    (merge-with (partial merge-with merge-sets) wo-from-events wo-from-plan))
 
+  ;; As long as our assumption holds true (just maps on every level, except leaves), we can do all of this way more concisely with the deep-merge function.
+  ;; That can be found in the docs for merge-with, so it's included in the utils namespace here.
+  ;; This will just do a merge on all maps found and will apply the provided function only if it encounters something that's not a map.
+  ;; Of course, this locks us into that particular structure, but so far this has only advantages.
+  (let [wo-from-events (get-in from-events [0])
+        wo-from-plan   (get-in plan-excerpt [0])]
+    (utils/deep-merge-with merge-sets wo-from-events wo-from-plan))
 
-
+;; Now for readability we omitted the top level: the mesocycle name.
 
   ()) ;;
  ;;)
